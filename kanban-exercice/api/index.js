1f3c9a565355767bfce953b0b50f3aee6aa57c82
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-12-28 19:39:06 +0100

    backend finished

--- kanban-exercice/api/index.js
+++ kanban-exercice/api/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-  
+var cors = require("cors");
+var dotenv = require("dotenv");
+var mongoose = require("mongoose");
+var cardRouter = require("./routes/Card")
+var columnRouter = require("./routes/Column")
 const app = express();
-const PORT = 3000;
-  
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Kanban Project!')
+})
+app.use("/card", cardRouter);
+app.use("/column", columnRouter);
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => app.listen(PORT, () => console.log(`Connected to database and running on port ${PORT}`)))
+    .catch((error) => console.log(error));
+ mongoose.set('strictQuery', false);
+module.exports = app;
---
+++ kanban-exercice/api/models/Card.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    assignee: {
+        type: String,
+        required: true,
+    },
+});
+
+const Card = mongoose.model("Card", cardSchema);
+
+exports.Card = Card;
+
